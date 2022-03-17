525898cd443e5a83717a61221e31756a88a1610f
Reid Offringa <reidoffringa@fb.com>
2022-03-17 14:17:38 -0400

    initial commit to github with a comment

---
+++ histogram.R
@@ -0,0 +1,10 @@
+require(ggplot2)
+
+df<-read.csv('./facebook/codesharingdemo/importantdata.csv')
+
+hist(df$important_data)
+
+
+ggplot(df, aes(x=important_data)) + 
+  geom_histogram(aes(y=..density..), colour="grey", fill="white", binwidth=2)+
+  geom_density(alpha=.2, fill="#FF6666") 
