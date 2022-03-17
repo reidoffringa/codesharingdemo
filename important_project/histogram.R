9081a465b23f36492528c717a7e9a056d1dff708
Reid Offringa <reidoffringa@fb.com>
2022-03-17 14:45:45 -0400

    updates to project

--- important_project/histogram.R
+++ important_project/histogram.R
@@ -4,7 +4,7 @@ df<-read.csv('./facebook/codesharingdemo/importantdata.csv')
 
 hist(df$important_data)
 
-
 ggplot(df, aes(x=important_data)) + 
   geom_histogram(aes(y=..density..), colour="grey", fill="white", binwidth=2)+
   geom_density(alpha=.2, fill="#FF6666") 
+
